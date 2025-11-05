bc73ec0ffadaf899318ad17eddcb5493b030255a
Abhinav Bansal <abhibansal60@gmail.com>
2025-11-05 15:57:45 +0530

    Improve build version metadata

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,4 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_APP_VERSION?: string;
+  readonly VITE_BUILD_TIMESTAMP?: string;
+  readonly VITE_COMMIT_REF?: string;
+  readonly VITE_DEPLOYMENT_ID?: string;
 }
